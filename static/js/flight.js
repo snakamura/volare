9548c83c2ea4020eb88e58888f5c7854f69d9006
Satoshi Nakamura <snak@snak.org>
2012-09-05 17:41:40 +0900

    Set viewport of the map to show all corrdinates in a flight

--- static/js/flight.js
+++ static/js/flight.js
@@ -1,13 +1,28 @@
+var LatLng = google.maps.LatLng;
+var LatLngBounds = google.maps.LatLngBounds;
+
 $(function() {
     var options = {
-        center: new google.maps.LatLng(records[0].latitude, records[0].longitude),
+        center: new LatLng(records[0].latitude, records[0].longitude),
         zoom: 14,
         mapTypeId: google.maps.MapTypeId.TERRAIN
     };
     var map = new google.maps.Map($('#map')[0], options);
 
+    var minLatitude = records[0].latitude;
+    var maxLatitude = records[0].latitude;
+    var minLongitude = records[0].longitude;
+    var maxLongitude = records[0].longitude;
+    $.each(records, function(index, record) {
+        minLatitude = Math.min(minLatitude, record.latitude);
+        maxLatitude = Math.max(maxLatitude, record.latitude);
+        minLongitude = Math.min(minLongitude, record.longitude);
+        maxLongitude = Math.max(maxLongitude, record.longitude);
+    });
+    map.fitBounds(new LatLngBounds(new LatLng(maxLatitude, minLongitude), new LatLng(minLatitude, maxLongitude)));
+
     var path = new google.maps.MVCArray($.map(records, function(record) {
-        return new google.maps.LatLng(Math.round(record.latitude*10000)/10000, Math.round(record.longitude*10000)/10000);
+        return new LatLng(record.latitude, record.longitude);
     }));
     var polyline = new google.maps.Polyline({
         map: map,
