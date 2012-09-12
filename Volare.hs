b41f1e24059bd3a4559dc77b5067bc30ccb0cf5a
Satoshi Nakamura <snak@snak.org>
2012-09-12 15:57:10 +0900

    Move Volare definition to Volare.Foundation

--- Volare.hs
+++ Volare.hs
@@ -6,7 +6,6 @@ module Volare (
 import qualified Codec.IGC as IGC
 import Control.Applicative ((<$>))
 import Control.Exception.Lifted (handle)
-import Control.Monad.Logger (LogLevel(LevelDebug))
 import Control.Monad.IO.Class (liftIO)
 import Data.Aeson ((.=))
 import qualified Data.Aeson as JSON
@@ -29,26 +28,18 @@ import Database.Persist (Entity(Entity),
                          insert,
                          update,
                          selectList)
-import Database.Persist.GenericSql (SqlPersist,
-                                    runMigration)
-import Database.Persist.Store (PersistConfigPool,
-                               applyEnv,
+import Database.Persist.GenericSql (runMigration)
+import Database.Persist.Store (applyEnv,
                                createPoolConfig,
                                loadConfig,
                                runPool)
 import Network.Wai (Application)
 import Network.Wai.Middleware.RequestLogger (logStdout)
 import System.Locale (defaultTimeLocale)
-import Text.Blaze.Html (Html)
 import Text.Printf (printf)
-import Text.Shakespeare.I18N (RenderMessage,
-                              renderMessage)
-import Web.ClientSession (getKey)
 import Yesod.Core (Yesod(..),
-                   clientSessionBackend,
                    defaultLayout,
                    logDebug,
-                   renderRoute,
                    yesodDispatch)
 import Yesod.Content (RepHtml,
                       RepHtmlJson)
@@ -58,16 +49,12 @@ import Yesod.Default.Config (AppConfig,
                              appExtra,
                              fromArgs,
                              withYamlEnvironment)
-import Yesod.Dispatch (mkYesod,
-                       parseRoutesFile,
+import Yesod.Dispatch (mkYesodDispatch,
                        toWaiApp)
 import Yesod.Form (AForm,
                    Enctype,
-                   FormMessage,
                    FormResult(FormSuccess),
-                   MForm,
                    areq,
-                   defaultFormMessage,
                    fileAFormReq,
                    generateFormPost,
                    renderDivs,
@@ -81,7 +68,6 @@ import Yesod.Persist (YesodPersist(..),
 import Yesod.Request (FileInfo,
                       fileName,
                       fileSource)
-import Yesod.Static (Static)
 import Yesod.Widget (addScript,
                      addScriptRemote,
                      addStylesheet,
@@ -91,47 +77,14 @@ import Yesod.Widget (addScript,
 import Volare.Config (Config,
                       parseConfig)
 import qualified Volare.Config as Config
+import Volare.Foundation
 import qualified Volare.Model as M
-import Volare.Settings (PersistConfig,
-                        widgetFile)
+import Volare.Settings (widgetFile)
 import Volare.Static (staticSite)
 import qualified Volare.Static as S
 
 
-data Volare = Volare {
-    volareConfig         :: AppConfig DefaultEnv Config,
-    volarePersistConfig  :: PersistConfig,
-    volareConnectionPool :: PersistConfigPool PersistConfig,
-    volareStatic         :: Static
-}
-
-
-mkYesod "Volare" $(parseRoutesFile "config/routes")
-
-
-instance Yesod Volare where
-    logLevel _ = LevelDebug
-
-    makeSessionBackend _ = do
-      key <- getKey "config/client_session_key.aes"
-      return $ Just $ clientSessionBackend key 120
-
-
-instance YesodPersist Volare where
-    type YesodPersistBackend Volare = SqlPersist
-
-    runDB action = do
-      persistConfig <- volarePersistConfig <$> getYesod
-      pool <- volareConnectionPool <$> getYesod
-      runPool persistConfig action pool
-
-
-instance RenderMessage Volare FormMessage where
-    renderMessage _ _ = defaultFormMessage
-
-
-type Form a = Html ->
-              MForm Volare Volare (FormResult a, Widget)
+mkYesodDispatch "Volare" resourcesVolare
 
 
 getRootR :: Handler RepHtml
---
+++ Volare/Foundation.hs
@@ -0,0 +1,65 @@
+module Volare.Foundation where
+
+import Control.Applicative ((<$>))
+import Control.Monad.Logger (LogLevel(LevelDebug))
+import Database.Persist.GenericSql (SqlPersist)
+import Database.Persist.Store (PersistConfigPool,
+                               runPool)
+import Text.Blaze.Html (Html)
+import Text.Shakespeare.I18N (RenderMessage,
+                              renderMessage)
+import Web.ClientSession (getKey)
+import Yesod.Core (Yesod(..),
+                   clientSessionBackend,
+                   renderRoute)
+import Yesod.Default.Config (AppConfig,
+                             DefaultEnv)
+import Yesod.Dispatch (mkYesodData,
+                       parseRoutesFile)
+import Yesod.Form (FormMessage,
+                   FormResult,
+                   MForm,
+                   defaultFormMessage)
+import Yesod.Handler (getYesod)
+import Yesod.Persist (YesodPersist(..))
+import Yesod.Static (Static)
+
+import Volare.Config (Config)
+import qualified Volare.Model as M
+import Volare.Settings (PersistConfig)
+
+
+data Volare = Volare {
+    volareConfig         :: AppConfig DefaultEnv Config,
+    volarePersistConfig  :: PersistConfig,
+    volareConnectionPool :: PersistConfigPool PersistConfig,
+    volareStatic         :: Static
+}
+
+
+mkYesodData "Volare" $(parseRoutesFile "config/routes")
+
+
+instance Yesod Volare where
+    logLevel _ = LevelDebug
+
+    makeSessionBackend _ = do
+      key <- getKey "config/client_session_key.aes"
+      return $ Just $ clientSessionBackend key 120
+
+
+instance YesodPersist Volare where
+    type YesodPersistBackend Volare = SqlPersist
+
+    runDB action = do
+      persistConfig <- volarePersistConfig <$> getYesod
+      pool <- volareConnectionPool <$> getYesod
+      runPool persistConfig action pool
+
+
+instance RenderMessage Volare FormMessage where
+    renderMessage _ _ = defaultFormMessage
+
+
+type Form a = Html ->
+              MForm Volare Volare (FormResult a, Widget)
