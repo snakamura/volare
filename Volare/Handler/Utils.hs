2a9593e83349f68673a497e92867ff146fcad718
Satoshi Nakamura <snak@snak.org>
2013-03-12 00:01:29 +0900

    Update jQuery

--- Volare/Handler/Utils.hs
+++ Volare/Handler/Utils.hs
@@ -14,13 +14,13 @@ import qualified Volare.Static as S
 
 
 addJQuery :: Widget
-addJQuery = addScriptRemote "//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"
+addJQuery = addScriptRemote "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
 
 
 addJQueryUI :: Widget
 addJQueryUI = do
-  addScriptRemote "//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"
-  addStylesheetRemote $ "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/themes/ui-lightness/jquery-ui.css"
+  addScriptRemote "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/jquery-ui.min.js"
+  addStylesheetRemote $ "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/ui-lightness/jquery-ui.css"
 
 
 addUnderscore :: Widget
