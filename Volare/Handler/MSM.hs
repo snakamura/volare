5cfaa707a5ab08121e504bf05aac64f167e42a0e
Satoshi Nakamura <snak@snak.org>
2013-07-29 15:49:57 +0900

    Rename the handler

--- Volare/Handler/MSM.hs
+++ Volare/Handler/MSM.hs
@@ -1,5 +1,5 @@
 module Volare.Handler.MSM (
-    getMSMR
+    getSurfaceR
 ) where
 
 import Control.Applicative ((<$>))
@@ -22,29 +22,29 @@ import Volare.Foundation
 import qualified Volare.MSM as MSM
 
 
-getMSMR :: Int ->
-           Int ->
-           Int ->
-           Int ->
-           Handler JSON.Value
-getMSMR year month day hour = do
+getSurfaceR :: Int ->
+               Int ->
+               Int ->
+               Int ->
+               Handler JSON.Value
+getSurfaceR year month day hour = do
   nwLatitude <- (>>= readMaybe . T.unpack) <$> lookupGetParam "nwlat"
   nwLongitude <- (>>= readMaybe . T.unpack) <$> lookupGetParam "nwlon"
   seLatitude <- (>>= readMaybe . T.unpack) <$> lookupGetParam "selat"
   seLongitude <- (>>= readMaybe . T.unpack) <$> lookupGetParam "selon"
   case (nwLatitude, nwLongitude, seLatitude, seLongitude) of
     (Just nwLat, Just nwLon, Just seLat, Just seLon) -> do
-      path <- liftIO $ getMSM year month day
+      path <- liftIO $ dataFile year month day
       surfaces <- liftIO $ MSM.getSurface path (nwLat, nwLon) (seLat, seLon) hour
       return $ JSON.toJSON surfaces
     _ -> notFound
 
 
-getMSM :: Int ->
-          Int ->
-          Int ->
-          IO FilePath
-getMSM year month day = do
+dataFile :: Int ->
+            Int ->
+            Int ->
+            IO FilePath
+dataFile year month day = do
   let path = printf "./data/msm/s/%04d%02d%02d.nc" year month day
       url = printf "http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/gpv/netcdf/MSM-S/%04d/%02d%02d.nc" year month day
   b <- doesFileExist path
