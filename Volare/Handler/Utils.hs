a58f5a4094e9edf5ebb1f62517ac163c2599a348
Satoshi Nakamura <snak@snak.org>
2014-02-14 21:19:46 +0900

    Update jQuery and jQuery UI

--- Volare/Handler/Utils.hs
+++ Volare/Handler/Utils.hs
@@ -14,13 +14,13 @@ import qualified Volare.Static as S
 
 
 addJQuery :: Widget
-addJQuery = addScriptRemote "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
+addJQuery = addScriptRemote "//code.jquery.com/jquery-2.1.0.min.js"
 
 
 addJQueryUI :: Widget
 addJQueryUI = do
-  addScriptRemote "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/jquery-ui.min.js"
-  addStylesheetRemote $ "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/ui-lightness/jquery-ui.css"
+  addScriptRemote "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"
+  addStylesheetRemote $ "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/ui-lightness/jquery-ui.css"
 
 
 addBootstrap :: Widget
