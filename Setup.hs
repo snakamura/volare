74179185c9004cd19045040e2f913178fcb417d0
Satoshi Nakamura <snak@snak.org>
2014-03-26 17:49:02 +0900

    Build msm library from cabal

--- Setup.hs
+++ Setup.hs
@@ -2,17 +2,43 @@ module Main (main) where
 
 import Data.Maybe (fromJust)
 import qualified Distribution.PackageDescription as PD
-import Distribution.Simple (confHook,
+import Distribution.Simple (Args,
+                            confHook,
                             defaultMainWithHooks,
+                            postClean,
+                            preConf,
                             simpleUserHooks)
-import Distribution.Simple.LocalBuildInfo (localPkgDescr)
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo,
+                                           localPkgDescr)
+import Distribution.Simple.Setup (CleanFlags,
+                                  ConfigFlags,
+                                  cleanVerbosity,
+                                  configVerbosity,
+                                  fromFlag)
+import Distribution.Simple.Utils (rawSystemExit)
 import System.Directory (getCurrentDirectory)
 
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks { confHook = volareConfHook }
+main = defaultMainWithHooks simpleUserHooks {
+           preConf = volarePreConf,
+           confHook = volareConfHook,
+           postClean = volarePostClean
+       }
 
 
+volarePreConf :: Args ->
+                 ConfigFlags ->
+                 IO PD.HookedBuildInfo
+volarePreConf args flags = do
+    buildInfo <- preConf simpleUserHooks args flags
+    rawSystemExit (fromFlag $ configVerbosity flags) "/bin/sh" ["-c", "cd msm && make"]
+    return buildInfo
+
+
+volareConfHook :: (PD.GenericPackageDescription, PD.HookedBuildInfo) ->
+                  ConfigFlags ->
+                  IO LocalBuildInfo
 volareConfHook (description, buildInfo) flags = do
     localBuildInfo <- confHook simpleUserHooks (description, buildInfo) flags
     let packageDescription = localPkgDescr localBuildInfo
@@ -28,3 +54,13 @@ volareConfHook (description, buildInfo) flags = do
             }
         }
     }
+
+
+volarePostClean :: Args ->
+                   CleanFlags ->
+                   PD.PackageDescription ->
+                   () ->
+                   IO ()
+volarePostClean args flags description _ = do
+    rawSystemExit (fromFlag $ cleanVerbosity flags) "/bin/sh" ["-c", "cd msm && make clean"]
+    postClean simpleUserHooks args flags description ()
