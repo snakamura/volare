d7569376e71ecb71408db7dd0713c832f57cc593
Satoshi Nakamura <snak@snak.org>
2014-05-06 15:13:30 +0900

    Add a test to parse an invalid wpt

--- test/GeoWptSpec.hs
+++ test/GeoWptSpec.hs
@@ -1,13 +1,17 @@
 module GeoWptSpec (spec) where
 
 import Control.Monad (void)
-import Control.Monad.Trans.State.Strict (evalStateT)
+import Control.Monad.Trans.State.Strict
+    ( evalStateT
+    , execStateT
+    )
 import Data.Maybe
     ( fromJust
     , isJust
     , isNothing
     )
 import qualified Pipes as P
+import qualified Pipes.Attoparsec as P
 import qualified Pipes.ByteString as P
 import System.IO
     ( IOMode(ReadMode)
@@ -16,12 +20,14 @@ import System.IO
 import Test.Hspec
 
 import qualified Codec.GeoWpt as GeoWpt
+import Codec.Utils.Attoparsec (line)
 
 
 spec :: Spec
 spec = do
-    let load = withFile "test/test.wpt" ReadMode $ \handle -> do
-                   wpt <- evalStateT GeoWpt.parser $ P.fromHandle handle
+    let withTestFile action = withFile "test/test.wpt" ReadMode $ action . P.fromHandle
+    let load = withTestFile $ \producer -> do
+                   wpt <- evalStateT GeoWpt.parser producer
                    wpt `shouldSatisfy` isJust
                    return $ fromJust wpt
 
@@ -30,8 +36,14 @@ spec = do
             void load
 
         it "fails with extra inputs" $ do
-            withFile "test/test.wpt" ReadMode $ \handle -> do
-                wpt <- evalStateT GeoWpt.parser $ P.fromHandle handle >> P.yield "\n"
+            withTestFile $ \producer -> do
+                wpt <- evalStateT GeoWpt.parser $ producer >> P.yield "\n"
+                wpt `shouldSatisfy` isNothing
+
+        it "fails without the first line" $ do
+            withTestFile $ \producer -> do
+                rest <- execStateT (P.parse line) producer
+                wpt <- evalStateT GeoWpt.parser rest
                 wpt `shouldSatisfy` isNothing
 
     context "when load from a file" $ do
