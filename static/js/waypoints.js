c6ecb23f01f986bebf0f0efcb59f2a0925d177d4
Satoshi Nakamura <snak@snak.org>
2014-10-15 16:10:21 +0900

    Handle DnD using a directive

--- static/js/waypoints.js
+++ static/js/waypoints.js
@@ -2,7 +2,15 @@
     var app = angular.module('Waypoints', []);
 
     app.controller('WaypointsController', ['$scope', '$http', function($scope, $http) {
-        function addFiles(files) {
+        $scope.waypoints = [];
+        $scope.addWaypoint = function(waypoint) {
+            var index = _.sortedIndex(this.waypoints, waypoint, function(waypoint) {
+                return waypoint.name;
+            });
+            this.waypoints.splice(index, 0, waypoint);
+        };
+        $scope.addFiles = function(files) {
+            var self = this;
             _.each(files, function(file) {
                 var reader = new FileReader();
                 $(reader).on('loadend', function(event) {
@@ -13,53 +21,52 @@
                         if (files.length == 1)
                             document.location.href = '/waypoints/' + waypoint.id;
                         else
-                            $scope.addWaypoint(waypoint);
+                            self.addWaypoint(waypoint);
                     });
                 });
                 reader.readAsText(file);
             });
-        }
-
-        $scope.waypoints = [];
-        $scope.addWaypoint = function(waypoint) {
-            var index = _.sortedIndex(this.waypoints, waypoint, function(waypoint) {
-                return waypoint.name;
-            });
-            this.waypoints.splice(index, 0, waypoint);
         };
-        $scope.addFiles = addFiles;
 
         $http.get('/waypoints').success(function(waypoints) {
             $scope.waypoints = waypoints;
         });
-
-        var $dropTarget = $('#waypoints');
-        $dropTarget.on('dragenter', function(event) {
-            event.preventDefault();
-            event.originalEvent.dataTransfer.dropEffect = 'copy';
-        });
-        $dropTarget.on('dragleave', function(event) {
-            event.preventDefault();
-        });
-        $dropTarget.on('dragover', function(event) {
-            event.preventDefault();
-        });
-        $dropTarget.on('drop', function(event) {
-            event.preventDefault();
-            addFiles(event.originalEvent.dataTransfer.files);
-        });
     }]);
 
-    app.directive('volareFile', ['$parse', '$timeout', function($parse, $timeout) {
+    app.directive('volareFile', ['$parse', function($parse) {
         return {
             link: function(scope, element, attrs) {
                 var handler = $parse(attrs['volareFile'])
                 element.bind('change', function(event) {
-                    $timeout(function() {
-                        handler(scope, {
-                            $event: event,
-                            $files: event.target.files
-                        });
+                    handler(scope, {
+                        $event: event,
+                        $files: event.target.files
+                    });
+                });
+            }
+        };
+    }]);
+
+    app.directive('volareDrop', ['$parse', function($parse) {
+        return {
+            link: function(scope, element, attrs) {
+                var handler = $parse(attrs['volareDrop'])
+                element.bind('dragenter', function(event) {
+                    event.preventDefault();
+                    event.originalEvent.dataTransfer.dropEffect = 'copy';
+                });
+                element.bind('dragleave', function(event) {
+                    event.preventDefault();
+                });
+                element.bind('dragover', function(event) {
+                    event.preventDefault();
+                });
+                element.bind('drop', function(event) {
+                    console.log(event.originalEvent.dataTransfer.files);
+                    event.preventDefault();
+                    handler(scope, {
+                        $event: event,
+                        $files: event.originalEvent.dataTransfer.files
                     });
                 });
             }
