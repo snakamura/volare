34eba976ca2f302d387d351148c1670370577ee8
Satoshi Nakamura <snak@snak.org>
2014-10-21 23:25:12 +0900

    Use directive to show waypoints

--- static/js/waypoint.js
+++ static/js/waypoint.js
@@ -1,8 +1,15 @@
 (function() {
     var waypoint = angular.module('volare.waypoint', ['volare.name']);
 
-    waypoint.controller('WaypointNameController', ['$scope', '$http', '$name', function($scope, $http, $name) {
-        $scope.name = $name;
+    waypoint.controller('WaypointController', ['$scope', '$http', function($scope, $http) {
+        $scope.items = [];
+        $http.get('').success(function(waypoint) {
+            $scope.name = waypoint.name;
+            $scope.items = waypoint.items;
+        });
+    }]);
+
+    waypoint.controller('WaypointNameController', ['$scope', '$http', function($scope, $http) {
         $scope.update = function(name) {
             var self = this;
             $http.put('', {
@@ -19,45 +26,60 @@
             }
         };
     }]);
-}());
 
-$(function() {
-    var $map = $('#map');
-    var map = new google.maps.Map($map[0], {
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    });
+    waypoint.directive('volareWaypoint', [function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div></div>',
+            link: function(scope, element, attrs) {
+                var map = new google.maps.Map(element[0], {
+                    mapTypeId: google.maps.MapTypeId.HYBRID
+                });
 
-    function layout() {
-        $map.width($(document).width());
-        var mapPosition = $map.position();
-        $map.height($(document).height() - mapPosition.top);
-    }
-    $(window).on('resize', layout);
-    layout();
+                var markers = [];
+                scope.$watch('items', function(items) {
+                    _.each(markers, function(marker) {
+                        marker.setMap(null);
+                    });
+                    markers = [];
 
-    $.getJSON('', function(waypoint) {
-        var bounds = null;
-        _.each(waypoint.items, function(item) {
-            var position = new google.maps.LatLng(item.latitude, item.longitude);
-            var label = item.name;
-            if (item.name !== item.description) {
-                label += ' (' + item.description + ')';
+                    if (items.length !== 0) {
+                        var bounds = null;
+                        _.each(scope.items, function(item) {
+                            var position = new google.maps.LatLng(item.latitude, item.longitude);
+                            var label = item.name;
+                            if (item.name !== item.description) {
+                                label += ' (' + item.description + ')';
+                            }
+                            var marker = new MarkerWithLabel({
+                                map: map,
+                                position: position,
+                                title: item.name,
+                                labelContent: label,
+                                labelAnchor: new google.maps.Point(-15, 35),
+                                labelClass: 'label'
+                            });
+                            markers.push(marker);
+
+                            if (!bounds)
+                                bounds = new google.maps.LatLngBounds(position, position);
+                            else
+                                bounds.extend(position);
+                        });
+
+                        map.fitBounds(bounds);
+                    }
+                });
+
+                function layout() {
+                    element.width($(document).width());
+                    var mapPosition = element.position();
+                    element.height($(document).height() - mapPosition.top);
+                }
+                $(window).on('resize', layout);
+                layout();
             }
-            var marker = new MarkerWithLabel({
-                map: map,
-                position: position,
-                title: item.name,
-                labelContent: label,
-                labelAnchor: new google.maps.Point(-15, 35),
-                labelClass: 'label'
-            });
-
-            if (!bounds)
-                bounds = new google.maps.LatLngBounds(position, position);
-            else
-                bounds.extend(position);
-        });
-
-        map.fitBounds(bounds);
-    });
-});
+        };
+    }]);
+}());
