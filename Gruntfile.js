986bd74b88e6cdb3718363db23d04aae9cb0a8a6
Satoshi Nakamura <snak@snak.org>
2014-10-28 08:22:34 +0900

    Configure requirejs

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,6 +35,8 @@ module.exports = function(grunt) {
                     findNestedDependencies: true,
                     removeCombined: true,
                     optimizeCss: 'standard',
+                    preserveLicenseComments: false,
+                    fileExclusionRegExp: /\.git/,
                     paths: {
                         'angular': 'empty:',
                         'bootstrap': 'empty:',
