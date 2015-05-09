1cab01c22937c9f2205bc31bdac8dd39c134606f
Satoshi Nakamura <snak@snak.org>
2015-05-09 17:28:55 +0900

    Add a map

--- static/volare/forcast/index.js
+++ static/volare/forcast/index.js
@@ -1,9 +1,31 @@
 define([
-    'angular'
-], function(angular) {
+    'angular',
+    'google',
+    'volare/util/layout'
+], function(angular, google) {
     'use strict';
 
-    var module = angular.module('volare.forcast', []);
+    var module = angular.module('volare.forcast', [
+        'volare.util.layout'
+    ]);
+
+    module.controller('ForcastController', [function() {
+    }]);
+
+    module.directive('volareForcast', [function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div></div>',
+            link: function(scope, element, attrs) {
+                var map = new google.maps.Map(element[0], {
+                    mapTypeId: google.maps.MapTypeId.HYBRID,
+                    center: new google.maps.LatLng(35.685124, 139.752787),
+                    zoom: 6
+                });
+            }
+        };
+    }]);
 
     return module;
 });
