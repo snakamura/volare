0ebc66fb6cdd9163e5d122e5d9f5b1f49e3311d6
Satoshi Nakamura <snak@snak.org>
2015-05-29 12:24:08 +0900

    Update Javascript libraries

--- static/config.js
+++ static/config.js
@@ -4,15 +4,15 @@
     require.config({
         baseUrl: '/static',
         paths: {
-            'angular': '//ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min',
-            'angular-ui-bootstrap': '//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.12.1/ui-bootstrap-tpls.min',
+            'angular': '//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min',
+            'angular-ui-bootstrap': '//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.13.0/ui-bootstrap-tpls.min',
             'async': '//cdnjs.cloudflare.com/ajax/libs/requirejs-plugins/1.0.3/async.min',
-            'bootstrap': '//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min',
+            'bootstrap': '//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min',
             'domReady': '//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min',
             'google': 'ext/google',
-            'jquery': '//code.jquery.com/jquery-2.1.3.min',
-            'jquery-ui': '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min',
-            'lodash': '//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.3.1/lodash.min',
+            'jquery': '//code.jquery.com/jquery-2.1.4.min',
+            'jquery-ui': '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min',
+            'lodash': '//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.9.3/lodash.min',
             'markerwithlabel': 'lib/easy-markerwithlabel/markerwithlabel',
             'text': '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
             'underscore.string': '//cdnjs.cloudflare.com/ajax/libs/underscore.string/3.0.3/underscore.string.min'
