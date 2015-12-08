e28db1e32432d42fc385cae957f2f80dc071ba4d
Satoshi Nakamura <snak@snak.org>
2015-12-09 00:02:05 +0900

    Update JavaScript libraries

--- static/config.js
+++ static/config.js
@@ -4,18 +4,18 @@
     require.config({
         baseUrl: '/static',
         paths: {
-            'angular': '//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min',
-            'angular-ui-bootstrap': '//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.13.0/ui-bootstrap-tpls.min',
+            'angular': '//ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min',
+            'angular-ui-bootstrap': '//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.14.3/ui-bootstrap-tpls.min',
             'async': '//cdnjs.cloudflare.com/ajax/libs/requirejs-plugins/1.0.3/async.min',
-            'bootstrap': '//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min',
+            'bootstrap': '//netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min',
             'domReady': '//cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min',
             'google': 'ext/google',
             'jquery': '//code.jquery.com/jquery-2.1.4.min',
             'jquery-ui': '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min',
-            'lodash': '//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.9.3/lodash.min',
+            'lodash': '//cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.min',
             'markerwithlabel': 'lib/easy-markerwithlabel/markerwithlabel',
             'text': '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
-            'underscore.string': '//cdnjs.cloudflare.com/ajax/libs/underscore.string/3.0.3/underscore.string.min'
+            'underscore.string': '//cdnjs.cloudflare.com/ajax/libs/underscore.string/3.2.2/underscore.string.min'
         },
         shim: {
             'angular': {
