eb88267c795bc50510167a433b75e81b8d985d99
Satoshi Nakamura <snak@snak.org>
2019-05-19 22:32:02 +0900

    Fix some doctests are not tested

--- test/doctest.hs
+++ test/doctest.hs
@@ -5,5 +5,11 @@ import Test.DocTest (doctest)
 
 main :: IO ()
 main = doctest [ "-isrc"
+               , "-XOverloadedStrings"
+               , "-XRankNTypes"
+               , "-XScopedTypeVariables"
                , "src/Codec/IGC/Utils.hs"
+               , "src/Codec/Utils/Pipes.hs"
+               , "src/Service/UAS/Parser.hs"
+               , "src/Service/WINDAS/Parser.hs"
                ]
